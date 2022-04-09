12ac280e3741d286a9c6fcd17a2e451d30b9e9f2
yo-young <kimyj7331@gmail.com>
2022-04-09 22:48:01 +0900

    Update main.java

--- sp5-chap02/src/main/java/chap02/main.java
+++ sp5-chap02/src/main/java/chap02/main.java
@@ -6,8 +6,8 @@ public class main {
 	
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx =
-				new AnnotationConfigApplicationContext(AppContext.class);
-		Greeter g = ctx.getBean("greeter", Greeter.class);
+				new AnnotationConfigApplicationContext(AppContext.class); // AppContext의 설정 정보를 이용하여 Bean 객체를 생성한다.
+		Greeter g = ctx.getBean("greeter", Greeter.class); // Bean 객체를 제공한다.
 		String msg = g.greet("스프링");
 		System.out.println(msg);
 		ctx.close();
